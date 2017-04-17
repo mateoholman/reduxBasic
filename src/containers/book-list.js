ee19754863faac86648403d1bb213c163b35518c
Matthew Holman <mateoholman@gmail.com>
2017-04-17 14:43:15 -0500

    Created the book-list container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { conntect } from 'react-redux';
+
 export default class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
@@ -17,3 +19,12 @@ export default class BookList extends Component {
     )
   }
 }
+
+function mapStateToProps(state) {
+  //Whatever is returned from here will show-up as props in BookList.
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);

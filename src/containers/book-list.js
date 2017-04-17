5a22729f878d073347d4ddf79119e8bed8de55af
Matthew Holman <mateoholman@gmail.com>
2017-04-17 14:51:31 -0500

    Bug fixes in book-list container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-import { conntect } from 'react-redux';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return(
-        <li key="book.title" className="list-group-item">{book.title}</li>
+        <li key={book.title} className="list-group-item">{book.title}</li>
       );
     });
   }

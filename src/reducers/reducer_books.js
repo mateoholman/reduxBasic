cff73e9dc7b8bd09a4ae900beae87b882395f5b4
Matthew Holman <mateoholman@gmail.com>
2017-04-24 12:59:01 -0500

    connected book reducer + book detail

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'JavaScript the good parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Gunslinger'},
-    { title: 'No Country for Old Men'}
+    { title: 'JavaScript the good parts', pages: 100},
+    { title: 'Harry Potter', pages: 500},
+    { title: 'The Gunslinger', pages: 600},
+    { title: 'No Country for Old Men', pages: 250}
   ]
 }

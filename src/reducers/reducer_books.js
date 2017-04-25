a425ecd5a75ede67dbd05b0b28315e3835488445
Matthew Holman <mateoholman@gmail.com>
2017-04-25 17:28:41 -0500

    Added more data to the books object

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,6 +3,8 @@ export default function() {
     { title: 'JavaScript the good parts', pages: 100},
     { title: 'Harry Potter', pages: 500},
     { title: 'The Gunslinger', pages: 600},
-    { title: 'No Country for Old Men', pages: 250}
+    { title: 'No Country for Old Men', pages: 250},
+    { title: 'Everybody Poops', pages: 20},
+    { title: 'Asops Fables', pages: 230}
   ]
 }
